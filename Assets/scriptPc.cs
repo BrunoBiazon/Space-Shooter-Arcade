49b5904e74dafe8a5c613e5c3f2035383fec1df9
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-02 08:49:46 -0300

    feat: Implementação lógica para definir altura e largura da tela, por meio do Camera.main | Adição de posição inicial para a NavePC

--- Assets/scriptPc.cs
+++ Assets/scriptPc.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class scriptPc : MonoBehaviour { 
 
     private Rigidbody2D rbd;
     public float velocidade;
+    private float altura, largura;
 
     void Start(){
-
+        transform.position = new Vector2(0, -3.5f);
         rbd = this.GetComponent<Rigidbody2D>();
         velocidade = 10;
+        altura = Camera.main.orthographicSize;
+        largura = altura * Camera.main.aspect;
     }
 
     void Update(){
@@ -21,10 +25,10 @@ public class scriptPc : MonoBehaviour {
 
         rbd.velocity = new Vector2(x, y) * velocidade; 
 
-        if (this.transform.position.x > 11.4)
-            this.transform.position = new Vector2(-11.4f, this.transform.position.y);
+        if (this.transform.position.x > largura)
+            this.transform.position = new Vector2(-largura, this.transform.position.y);
         else if(this.transform.position.x < -11.4)
-            this.transform.position = new Vector2(11.4f, this.transform.position.y);
+            this.transform.position = new Vector2(largura, this.transform.position.y);
 
         if (this.transform.position.y > -0.30)
             this.transform.position = new Vector2(this.transform.position.x, -0.30f);
