81da91c7d3528b054e964da2e363adfbc69b0bbf
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-02 10:40:44 -0300

    Feat ( scriptTiro): Implentação inicial, somente velocidade do objeto e autodestruição ao passar da altura limite;

---
+++ Assets/scriptTiro.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptTiro : MonoBehaviour
+{
+    Rigidbody2D rdb;
+    public float vel_tiro;
+    void Start()
+    {
+
+        rdb = GetComponent<Rigidbody2D>();
+        vel_tiro = 11;
+        rdb.velocity = new Vector2(0, vel_tiro);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > CameraUtils.GetAltura())
+        {
+
+            Destroy(this.gameObject);
+
+        }
+    }
+}
