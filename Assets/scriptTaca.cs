25d91e9e0590950c88f0b28c4aba70056f5b6a75
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-02 09:30:32 -0300

    feat: Script inicial do obstáculo ( Taça copa )

---
+++ Assets/scriptTaca.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptTaca : MonoBehaviour{
+
+    private Rigidbody2D rbd;
+    float velocidade = 5;
+
+    void Start(){
+        
+        rbd = this.GetComponent<Rigidbody2D>();
+        rbd.velocity = new Vector2(0, -velocidade);
+
+    }
+
+    void Update(){
+        
+        
+
+
+    }
+}
