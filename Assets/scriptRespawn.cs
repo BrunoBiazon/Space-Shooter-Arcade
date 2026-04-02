ff2235922683679101139eed1834180b5aad1bdd
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-02 10:17:27 -0300

    Feat(ScriptRespawn): Lógica na função respawn: - Permite que o objeto taça seja respawnado de maneira aleatória; - Garanti que o objeto sempre respawne por inteiro dentro da cena, por meio da margem do objeto;

--- Assets/scriptRespawn.cs
+++ Assets/scriptRespawn.cs
@@ -6,15 +6,22 @@ public class scriptRespawn : MonoBehaviour{
 
     public GameObject taca;
 
+    float larguraObjeto;
+    float largura;
     void Start()    {
 
+        largura = CameraUtils.GetLargura();
+        larguraObjeto = taca.GetComponent<SpriteRenderer>().bounds.extents.x;
         InvokeRepeating("respawnar", 0, 1);
 
     }
 
     private void respawnar()
     {
-        Vector2 pos = new Vector2(0, 5);
+        float posX;
+        posX = Random.Range(-largura + larguraObjeto, largura - larguraObjeto); 
+
+        Vector2 pos = new Vector2(posX, 5);
 
         Instantiate(taca, pos, Quaternion.identity);
 
