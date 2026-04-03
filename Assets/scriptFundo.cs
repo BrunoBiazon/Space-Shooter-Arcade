10c440aa7f8bfc90919fe52633cc28b55560a175
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-03 18:05:30 -0300

    Feat : Criei o scriptFundo, para reposicionar estilo "rolo": - Quando o cenário chega na alturaLimite da cena * 2, ou seja, ele sai por completo, é reposicionado para o topo do cenário;

---
+++ Assets/scriptFundo.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class scriptFundo : MonoBehaviour
+{
+    Rigidbody2D rgb;
+    float velocidade, altura, limiteInferior;
+
+    void Start()
+    {
+        velocidade = 4;
+        rgb = GetComponent<Rigidbody2D>();
+        rgb.velocity = new Vector2(0, -velocidade);
+
+        altura = CameraUtils.GetAltura();
+
+        limiteInferior = -altura * 2;
+    }
+
+    void Update()
+    {
+        if (transform.position.y < limiteInferior)
+        {
+            transform.position = new Vector2(0, altura );
+        }
+    }
+}
